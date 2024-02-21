fac9b56dd345bad97278f2b72af792a5338d925c
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-02-21 23:10:17 +0530

    first commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 
+
+
 function Todo() {
-  return (
-    <div className=' md:container md:mx-auto min-h-screen w-full bg-slate-500 '>
+  return ( <div className=' md:container md:mx-auto min-h-[80vh] w-full bg-slate-500 '>
       <div className=''>    
       <h1 className=' text-center text-3xl p-2'>iTask - Manage your todos at one place</h1>
       <div className=' flex flex-col items-center gap-2'>
@@ -16,6 +17,16 @@ function Todo() {
           <input type="checkbox" />
           <label >Show Finished</label>
         </div>
+      </div>
+      <h2>your Todos</h2>
+      <div className=' flex gap-2'>
+<div>
+  Lorem ipsum dolor sit amet.
+</div>
+<div className=' flex gap-2'>
+  <button>edit</button>
+  <button>delete</button>
+</div>
       </div>
       </div>
     </div>
