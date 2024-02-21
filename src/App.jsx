36122da6fd55c1d7c85391f2c170a83c65b41c34
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-02-21 10:50:58 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Todo from './components/Todo'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <Todo/>
     </>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 
 function Todo() {
   return (
-    <div>Todo</div>
+    <div className=' md:container md:mx-auto min-h-screen w-full bg-slate-500 '>
+      <div className=''>    
+      <h1 className=' text-center text-3xl p-2'>iTask - Manage your todos at one place</h1>
+      <div className=' flex flex-col items-center gap-2'>
+        <h2>Add a Todo</h2>
+        <div>
+          <input type="text" placeholder='write something here' />
+          <button>save</button>
+        </div>
+        <div> 
+          <input type="checkbox" />
+          <label >Show Finished</label>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
